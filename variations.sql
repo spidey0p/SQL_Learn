6edf61fbfdd3f36e660d1eb37ad4db2913b014a9
spidey0p <jyotiradityabiswal64@gmail.com>
2024-09-07 15:23:50 +0530

    added variation.sql

---
+++ variations.sql
@@ -0,0 +1,42 @@
+insert into externalsubjects values
+ (2, 'INFO2120 Database Systems 1', 'The University of Sydney', 2005, 5774);
+insert into variations values
+ (46233229, 554, 5774, 'advstanding', null, 2, 2005, 65, 5035569, '2007-03-03');
+
+
+insert into variations values
+ (46202887, 554, 5766, 'substitution', 5769, null, 2008, 50, 5035569, '2009-01-01');
+
+insert into variations values
+ (46239820, 547, 5766, 'substitution', 5769, null, 2007, 57, 5035569, '2009-01-01');
+
+
+insert into variations values
+ (46277566, 700, 5774, 'advstanding', null, 2, 2008, 60, 5035569, '2009-03-03');
+
+
+insert into variations values
+ (46292646, 554, 5774, 'exemption', null, 2, 2007, 55, 5035569, '2008-03-03');
+
+
+insert into externalsubjects values
+ (3, 'INFO1103 Intro to Programming', 'The University of Sydney', 2005, 5774);
+insert into externalsubjects values
+ (4, 'INFO1105 Data Structures', 'The University of Sydney', 2005, 5774);
+insert into variations values
+ (46354027, 554, 65511, 'advstanding', null, 3, 2007, 90, 5035569, '2008-03-03');
+insert into variations values
+ (46354027, 554, 65319, 'advstanding', null, 4, 2007, 85, 5035569, '2008-03-03');
+
+
+insert into variations values
+ (46276999, 554, 5766, 'substitution', 5769, null, 2007, 62, 5035569, '2009-01-01');
+insert into externalsubjects values
+ (5, 'INFO20003 Database Systems', 'The University of Melbourne', 2006, 5774);
+insert into variations values
+ (46276999, 554, 5774, 'advstanding', null, 5, 2006, 75, 5035569, '2008-03-03');
+insert into externalsubjects values
+ (6, 'INFO20004 Network Systems', 'The University of Melbourne', 2006, 5776);
+insert into variations values
+ (46276999, 554, 5776, 'exemption', null, 6, 2006, 76, 5035569, '2008-03-03');
+
